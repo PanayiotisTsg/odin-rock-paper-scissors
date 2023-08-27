a08f3930510e2c5911831e1242a75cf9221efb25
Panayiotis Tsangarides <139330855+PanayiotisTsg@users.noreply.github.com>
2023-08-27 17:01:55 +0300

    Add functionality to play a round

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function getComputerChoice() {
             break;
     }
 }
+
+// Return the winner based on the selections of
+// the player and the computer
+function playRound(playerSelection, computerSelection) {
+
+    // Convert first letter of playerSelection to uppercase and the rest to lowercase 
+    let firstPart = playerSelection[0].toUpperCase();
+    let secondPart = playerSelection.slice(1).toLowerCase();
+    playerSelection = firstPart + secondPart;
+
+    // Get the winner based on both choices
+    if (playerSelection === computerSelection) {
+        return 'It\'s a draw!';
+    } else if (playerSelection === 'Rock') {
+        return computerSelection === 'Paper'
+            ? 'You Lose! Paper beats Rock'
+            : 'You Win! Rock beats Scissors';
+    } else if (playerSelection === 'Paper') {
+        return computerSelection === 'Rock'
+            ? 'You Win! Paper beats Rock'
+            : 'You Lose! Scissors beats Paper';
+    } else {
+        return computerSelection === 'Rock'
+            ? 'You Lose! Rock beats Scissors'
+            : 'You Win! Scissors beats Paper';
+    }
+}
