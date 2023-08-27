addd903001ceea2537efb2a12cc3d7f01e922d5a
Panayiotis Tsangarides <139330855+PanayiotisTsg@users.noreply.github.com>
2023-08-27 17:51:16 +0300

    Add functionality to play a 5 round game
    User can prompt his choice for 5 rounds.
    Keep the score and declare a winner.

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function playRound(playerSelection, computerSelection) {
 
     // Get the winner based on both choices
     if (playerSelection === computerSelection) {
-        return 'It\'s a draw!';
+        return 'The round is a draw!';
     } else if (playerSelection === 'Rock') {
         return computerSelection === 'Paper'
             ? 'You Lose! Paper beats Rock'
@@ -45,3 +45,41 @@ function playRound(playerSelection, computerSelection) {
             : 'You Win! Scissors beats Paper';
     }
 }
+
+// Play a 5 round game - Keep the score - Report the winner
+function game() {
+    // Initialize player/computer points and declare selections
+    let playerPoints = 0;
+    let computerPoints = 0;
+    let roundWinner = '';
+    let playerSelection;
+    let computerSelection;
+
+    for (let i = 1; i <= 5; i++) {
+        // Set the player and computer selection for the round
+        playerSelection = prompt('Enter your choice: ');
+        computerSelection = getComputerChoice();
+
+        // Play a round and store the result (Round winner)
+        roundWinner = playRound(playerSelection, computerSelection);
+
+        // Check the 5th character of the result to identify the winner
+        // Add a point to the player/computer accordingly 
+        switch(roundWinner[4]) {
+            case 'W':
+                playerPoints++;
+                break;
+            case 'L':
+                computerPoints++;
+                break;
+        }
+        console.log(roundWinner);
+    }
+
+    // Check who got the most points and return the winner
+    return playerPoints > computerPoints ? 'You won the game!'
+        : playerPoints < computerPoints ? 'Computer won the game!'
+        : 'The game is a draw!'
+}
+
+console.log(game());
