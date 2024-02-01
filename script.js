3e7c604de8796d4a1fdeff0b75d45d1e5d64c5ad
Panayiotis Tsangarides <139330855+PanayiotisTsg@users.noreply.github.com>
2024-02-01 11:11:35 +0200

    Add round winner index and play again button
    1. Added colors to pc/player indexes for every round.
    2. Added play again button that resets the game
    3. Removed the ability to press any selection buttons
    when the game ends
    4. Update README.md

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function getComputerChoice() {
     }
 }
 
-// Return the winner based on the selections of
+// Return the round winner based on the selections of
 // the player and the computer
 function playRound(playerSelection, computerSelection) {
     let roundWinner;
@@ -49,6 +49,29 @@ function getScore(roundWinner) {
     else if (roundWinner === 'computer') computerPoints++;
 };
 
+// Display index colors based on the winner of the round
+function displayRWIndex(roundWinner) {
+    if (playerPoints === 0 && computerPoints === 0) {
+        playerIndex.style.border = GREY_BORDER;
+        pcIndex.style.border = GREY_BORDER;
+        return;
+    }
+
+    switch (roundWinner) {
+        case 'player':
+            playerIndex.style.border = GREEN_BORDER;
+            pcIndex.style.border = RED_BORDER;
+            break;
+        case 'computer':
+            pcIndex.style.border = GREEN_BORDER;
+            playerIndex.style.border = RED_BORDER;
+            break;
+        case 'draw':
+            playerIndex.style.border = GREY_BORDER;
+            pcIndex.style.border = GREY_BORDER;
+    }
+}
+
 function getSelectionEmoji(selection) {
     if (selection === 'rock') return String.fromCodePoint(9994);
     else if (selection === 'paper') return String.fromCodePoint(9995);
@@ -59,48 +82,96 @@ function getSelectionEmoji(selection) {
 function gameWinner() {
     if (playerPoints === 5) {
         winner.textContent = 'You Won!';
-        winner.style.cssText = 'border: 2px solid green;'
-        playerPoints = 0;
-        computerPoints = 0;
+        winner.style.border = GREEN_BORDER;
+        gameEnded = true;
+        return true;
     } else if (computerPoints === 5) {
         winner.textContent = 'You Lost!';
-        winner.style.cssText = 'border: 2px solid red;'
-        playerPoints = 0;
-        computerPoints = 0;
+        winner.style.border = RED_BORDER;
+        gameEnded = true;
+        return true;
     } else {
         winner.textContent = '';
-        winner.style.cssText = 'border: none;'
+        winner.style.border = 'none';
+        gameEnded = false;
+        return false;
     }
+}
+
+function displayPlayAgain() {
+    playAgainBtn.style.display = 'block';
+}
+
+function hidePlayAgain() {
+    playAgainBtn.style.display = 'none';
+}
+
+function playGame(e) {
+    const playerSelection = e.target.id;
+    const computerSelection = getComputerChoice();
+
+    //console.log(playerSelection);
+    //console.log(computerSelection);
+
+    playerSelectionImg.textContent = getSelectionEmoji(playerSelection);
+    computerSelectionImg.textContent = getSelectionEmoji(computerSelection);
+    
+    roundWinner = playRound(playerSelection, computerSelection);
+    getScore(roundWinner);
+
+    if (gameWinner()) displayPlayAgain();
+    else hidePlayAgain();
+
+    displayRWIndex(roundWinner);
+
+    playerScore.textContent = playerPoints;
+    computerScore.textContent = computerPoints;
+
+    if (gameEnded) {
+        selectButtons.forEach(btn => {
+            btn.removeEventListener('click', playGame)
+            btn.style.cursor = 'not-allowed';
+        });
+    }
+}
+
+function resetGame(e) {
+    playerPoints = 0;
+    computerPoints = 0;
+    playerScore.textContent = playerPoints;
+    computerScore.textContent = computerPoints;
+
+    hidePlayAgain();
+    gameWinner();
+    displayRWIndex();
+
+    selectButtons.forEach(btn => {
+        btn.addEventListener('click', playGame);
+        btn.style.cursor = 'pointer';
+    });
 }
 
 let playerPoints = 0;
 let computerPoints = 0;
 let roundWinner;
+let gameEnded;
 
-const buttons = document.querySelectorAll('button');
+const selectButtons = document.querySelectorAll('.select-btn');
 const playerScore = document.querySelector('.player');
 const computerScore = document.querySelector('.computer');
 const winner = document.querySelector('.winner');
 const playerSelectionImg = document.querySelector('.player-selection');
 const computerSelectionImg = document.querySelector('.computer-selection');
+const playerIndex = document.querySelector('.player-index');
+const pcIndex = document.querySelector('.pc-index');
+const playAgainBtn = document.querySelector('.play-again-btn');
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', e => {
-        const playerSelection = e.target.id;
-        const computerSelection = getComputerChoice();
+const GREY_BORDER = '2px solid #495964';
+const GREEN_BORDER = '2px solid green';
+const RED_BORDER = '2px solid red';
 
-        console.log(playerSelection);
-        console.log(computerSelection);
-
-        playerSelectionImg.textContent = getSelectionEmoji(playerSelection);
-        computerSelectionImg.textContent = getSelectionEmoji(computerSelection);
-        
-        roundWinner = playRound(playerSelection, computerSelection);
-        getScore(roundWinner);
-
-        gameWinner();
-
-        playerScore.textContent = playerPoints;
-        computerScore.textContent = computerPoints;
-    })
+selectButtons.forEach(btn => {
+    btn.addEventListener('click', playGame);
 });
+
+playAgainBtn.addEventListener('click', resetGame);
